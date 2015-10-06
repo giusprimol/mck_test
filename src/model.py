c0ac429d67cd6c51a61a8ee77c6c7a97b96061d4
giusp <giusprimol@hotmail.com>
2015-10-06 13:49:23 +0200

    versione intermedia

--- src/model.py
+++ src/model.py
@@ -1,74 +1,96 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Oct 05 22:31:39 2015
-
-Model file: collects the data items
-
+Model for data being loaded from disk. 
+Class deals with data loading and validation.
+Also, inherits from DataFrame_QAbstractTableModel so that it can emit signals 
+when data is updated
+    
 
 @author: giuseppe
 """
-import pandas as pd
-from PyQt4.QtCore import QAbstractTableModel,QVariant, Qt
 
+from model_pandas_dataframe import *
+from PyQt4.QtCore import Signal,QObject
+import os
+
+test_data = pd.DataFrame(index = ['A','B'], columns = ['2015','2016','2017'], data=[[100,101,102],[99,99,98]])
 
 calc_labels = ['A/(A+B)', 'B/(A+B)', 'A/B'] #row labels for calculated data
 threshold_labels = {False:'Basso', True:'Buono'} # labels for threshold comparison result
 
-perc_format = lambda x : "%4.1f%%"%(x*100) if isinstance(x,(long,int,float)) else x
+class loaded_data(QObject):
+    loaded_data_changed = Signal()    
+    def __init__(self, start_data = None):
+        super(loaded_data, self).__init__()        
+        self.data = start_data if start_data is not None else pd.DataFrame()
+        
+    def update_data(self,data):
+        self.validate_data(data)
+        self.data = data
+        self.loaded_data_changed.emit()
+        
+    def loadfile(self,filename):      
+        ext = os.path.splitext(filename)[1]
+        if ext in ('.txt','.csv'):
+            data = pd.DataFrame.from_csv(filename)
+        elif ext in ('*.'):
+            data = pd.read_excel(filename,index_col = 0)
+        else:
+            raise Exception('Estensione file sconosciuta: %s'%ext)
+        self.update_data(data)
 
-#loaded_data = pd.DataFrame(index = ['A','B'], columns = ['2015','2016','2017'], 
-#    data=[[100,101,102],[99,99,98]])
+    def validate_data(self,data):
+        if data.shape[1]<2:
+            raise Exception('Sono necessarie almeno 2 colonne di dati, il file ne contiene solo %d'%data.shape[1])
+        if data.index.duplicated().any():
+            raise Exception('Sono presenti serie con nome duplicato: %s'%', '.join(data.index[data.index.duplicated()]))
+        if data.shape[0]<2:
+            raise Exception('Sono necessarie almeno 2 serie di dati, il file ne contiene solo %d'%data.shape[0])
 
-class DataFrame_QAbstractTableModel(QAbstractTableModel):
-    def __init__(self, parent=None, *args):
-        QAbstractTableModel.__init__(self, parent, *args)
-        self.data = pd.DataFrame()
-    def rowCount(self, parent): return self.data.shape[0]
-    def columnCount(self, parent): return self.data.shape[1]
-    def data(self, index, role):
-        if not index.isValid():
-            return None
-        elif role != Qt.DisplayRole:
-            return None
-        return str(self.data.ix[index.row()][index.column()])
-    def headerData(self, section, orientation,role) :  
-        if (role == Qt.DisplayRole):  
-            if (orientation == Qt.Horizontal):  
-                return self.data.columns[section]
-            elif (orientation == Qt.Vertical):                   
-                return self.data.index[section]
-        return None
-
-
-class loaded_data_model(DataFrame_QAbstractTableModel):
-    def __init__(self, start_data = None,parent=None, *args):
-        DataFrame_QAbstractTableModel.__init__(self, parent, *args)
-        self.data = start_data
-
-class thresh_data_model(DataFrame_QAbstractTableModel):
-    def __init__(self, parent=None, *args):
-        DataFrame_QAbstractTableModel.__init__(self, parent, *args)
-        self.data = pd.DataFrame(index = calc_labels, columns = ['thr'], data = [0.01, 0.0, 0.01])
-
-class calc_data_model(DataFrame_QAbstractTableModel):
-    def __init__(self, loaded, thresh, parent=None, *args):
-        DataFrame_QAbstractTableModel.__init__(self, parent, *args)
-        self.loaded = loaded
-        self.thresh = thresh
-        self.update_data()
-    def update_data(self):
-        """
-        transforms loaded_data in calc_data if possible, otherwise returns None
-        """
-        if self.loaded.data is not None:
-            res = pd.DataFrame.from_items([
-                (calc_labels[0], self.loaded.data.ix['A']/self.loaded.data.sum()),
-                (calc_labels[1], self.loaded.data.ix['B']/self.loaded.data.sum()),
-                (calc_labels[2], self.loaded.data.ix['A']/self.loaded.data.ix['B'])]).T
-            res['CAGR'] = pd.np.power((res.ix[:,-1]/res.ix[:,0]), 1./(self.loaded.data.shape[1]-1))-1
-            res['Valutazione'] = (res.CAGR>self.thresh.data.thr).map(threshold_labels)
-            self.data = res
+class calc_data(QObject):
+    calc_data_changed = Signal()    
+    def __init__(self):
+        super(calc_data, self).__init__()       
+        self.data = pd.DataFrame()        
+        
+    def update_data(self, df_loaded_data, idxA, idxB):
+        if (df_loaded_data is not None) and (len(df_loaded_data)>0):        
+            tmp = pd.DataFrame({'A' : df_loaded_data.ix[idxA], 'B' : df_loaded_data.ix[idxB]}).T
+            self.data = pd.DataFrame.from_items([
+                (calc_labels[0], tmp.ix['A']/tmp.sum()),
+                (calc_labels[1], tmp.ix['B']/tmp.sum()),
+                (calc_labels[2], tmp.ix['A']/tmp.ix['B'])]).T
         else:
             self.data = pd.DataFrame()
+        self.calc_data_changed.emit()
+        
+class summary_data(QObject):
+    summary_data_changed = Signal()    
+    def __init__(self):
+        super(summary_data, self).__init__()       
+        self.data = pd.DataFrame()        
+    def update_data(self,df_calc_data, s_tresh):
+        if (df_calc_data is not None) and (len(df_calc_data)>0):
+            self.data = pd.DataFrame()
+            self.data['CAGR'] = pd.np.power((df_calc_data.ix[:,-1]/df_calc_data.ix[:,0]), 1./(df_calc_data.shape[1]-1))-1
+            self.data['Valutazione'] = (self.data.CAGR>s_tresh).map(threshold_labels)
+        else:
+            self.data = pd.DataFrame()
+        self.summary_data_changed.emit()
 
- 
+class thresh_data(QObject):
+    thresh_data_changed = Signal()    
+    def __init__(self):
+        super(thresh_data, self).__init__()       
+        self.data = pd.Series(dict(zip(calc_labels,[0,0,0])))
+        
+    def update_data(self,t1,t2,t3):
+        self.data = pd.Series(dict(zip(calc_labels,[t1,t2,t3])))
+        self.thresh_data_changed.emit()
+
+class model_data():
+    def __init__(self):
+        self.loaded_data = loaded_data()
+        self.calc_data = calc_data()
+        self.thresh_data = thresh_data()
+        self.summary_data = summary_data()
--- src/test.py
+++ src/test.py
@@ -5,59 +5,138 @@ Created on Mon Oct 05 21:28:15 2015
 @author: giuseppe
 """
 
-import pandas as pd
-import model,sys
+import pandas as pd, sys
 
-from pandas.sandbox.qtpandas import DataFrameModel, DataFrameWidget
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
-
+from PyQt4 import QtCore, QtGui, uic
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
 from matplotlib.figure import Figure
 
+from model import *
 
-test_data = pd.DataFrame(index = ['A','B'], columns = ['2015','2016','2017'], data=[[100,101,102],[99,99,98]])
-ld = model.loaded_data_model(test_data)
-td = model.thresh_data_model()
-cd = model.calc_data_model(ld,td)
+#==============================================================================
+# 
+# test_data = pd.DataFrame(index = ['A','B'], columns = ['2015','2016','2017'], data=[[100,101,102],[99,99,98]])
+# ld = model.loaded_data_model(test_data)
+# td = model.thresh_data_model()
+# cd = model.calc_data_model(ld,td)
+#==============================================================================
 
-from PyQt4 import QtCore, QtGui, uic
+
  
-form_class = uic.loadUiType("mainWin.ui")[0]                 # Load the UI
+# load the main, show and help windows
+mainwindow_class = uic.loadUiType("mainWin.ui")[0]
+#helpwindow_class = uic.loadUiType("helpWin.ui")[0]
+#showwindow_class = uic.loadUiType("showWin.ui")[0]
  
-class MyWindowClass(QtGui.QMainWindow, form_class):
-    def __init__(self, loaded_data, calc_data, thresh_data, parent=None):
+# Adding the matplotlib canvas and application logic
+class MyWindowClass(QtGui.QMainWindow, mainwindow_class):
+    def __init__(self, parent=None):
         QtGui.QMainWindow.__init__(self, parent)
+        self.model = model_data()
         self.setupUi(self)
-        self.fig = Figure()#(2.0, 4.0), dpi=100)
-        self.canvas = FigureCanvas(self.fig)
-        self.canvas.setParent(self.findChild(QtGui.QWidget,'widget_plot'))        
-        self.findChild(QtGui.QTableView,'table_calc_data').setModel(calc_data)
-        axes = self.fig.add_subplot(111)
-        cd.data[cd.data.columns[:-2]].T.plot(kind='bar', ax = axes)
-        axes.legend(ncol=3)
+        self.setupFigure()
+        self.setupSignals()
+    
+    def setupFigure(self):        
+        self.figure = Figure()#(2.0, 4.0), dpi=100)
+        self.canvas = FigureCanvas(self.figure)
+        self.canvas.setParent(self.findChild(QtGui.QWidget,'widget_plot'))
+                
+    def setupSignals(self):
+        # connecting signals from the data model...        
+        self.model.loaded_data.loaded_data_changed.connect(self.handle_loaded_data_changed)
+        self.model.calc_data.calc_data_changed.connect(self.handle_calc_data_changed)
+        self.model.thresh_data.thresh_data_changed.connect(self.handle_thresh_data_changed)
+        self.model.summary_data.summary_data_changed.connect(self.summary_data_data_changed)
+    
+        # ... and from the GUI
+        self.findChild(QtGui.QWidget,'but_load').clicked.connect(self.handle_press_loadData)
+        self.connect(self.findChild(QtGui.QWidget,'combo_serA'), QtCore.SIGNAL("currentIndexChanged(const QString&)"), 
+                    self.handle_combo_selection_change)
+        self.connect(self.findChild(QtGui.QWidget,'combo_serB'), QtCore.SIGNAL("currentIndexChanged(const QString&)"), 
+                    self.handle_combo_selection_change)
+        self.findChild(QtGui.QWidget,'spb_thr1').valueChanged.connect(self.handle_thresh_box_change)
+        self.findChild(QtGui.QWidget,'spb_thr2').valueChanged.connect(self.handle_thresh_box_change)
+        self.findChild(QtGui.QWidget,'spb_thr3').valueChanged.connect(self.handle_thresh_box_change)
+        
+
+    def handle_loaded_data_changed(self):
+        # update list boxes
+        data_series = sorted(self.model.loaded_data.data.index)
+        comboA = self.findChild(QtGui.QWidget,'combo_serA')
+        comboA.blockSignals(True)        
+        comboA.clear()
+        comboA.insertItems(0,data_series)
+        comboA.setCurrentIndex(0)
+        comboA.blockSignals(False)
+        
+        comboB = self.findChild(QtGui.QWidget,'combo_serB')
+        comboB.blockSignals(True)         
+        comboB.clear()
+        comboB.insertItems(0,data_series)
+        comboB.setCurrentIndex(1)
+        comboB.blockSignals(False)
+        self.handle_combo_selection_change(None)
+
+    
+    def handle_thresh_data_changed(self):
+        
+        pass
+    
+    def summary_data_data_changed(self):
+        # update interface
+        pass
+    
+    def handle_calc_data_changed(self):
+        # update summary data
+        # update interface
+        pass
+        
+    def handle_press_loadData(self):
+        try:        
+            (filename, filetype) = QtGui.QFileDialog.getOpenFileNameAndFilter(parent = None, 
+                caption = 'Select file...', directory = '.', 
+                filter= "Comma Separed Text (*.csv;*.txt);;Excel (*.xls;*.xlsm;*.xlsx)")
+            if filename:
+                self.model.loaded_data.loadfile(filename)
+        except Exception as e:
+            QtGui.QErrorMessage(self, 'Errore', e.message)
+    
+    def handle_combo_selection_change(self, dummy):
+        # update calc data
+        print "selChange"
+        comboA = self.findChild(QtGui.QWidget,'combo_serA')
+        comboB = self.findChild(QtGui.QWidget,'combo_serB')
+        self.model.calc_data.update_data(self.model.loaded_data.data, 
+            str(comboA.currentText()), str(comboB.currentText()))
+    
+    def handle_thresh_box_change(self, dummy):
+        self.model.summary_data.update_data(self.model.calc_data,        
+            self.findChild(QtGui.QWidget,'spb_thr1').value,
+            self.findChild(QtGui.QWidget,'spb_thr2').value,
+            self.findChild(QtGui.QWidget,'spb_thr3').value)
+        
+        
+        
+#==============================================================================
+#         self.findChild(QtGui.QTableView,'table_calc_data').setModel(calc_data)
+#         axes = self.fig.add_subplot(111)
+#         cd.data[cd.data.columns[:-2]].T.plot(kind='bar', ax = axes)
+#         axes.legend(ncol=3)
+#==============================================================================
 
         
         
         
 
 app = QtGui.QApplication(sys.argv)
-myWindow = MyWindowClass(ld,cd,td,None)
+myWindow = MyWindowClass(None)
 myWindow.show()
 app.exec_()
 
 
-
-
-
-
-def main():
-    app = QApplication(sys.argv)
-    w = MyWindow()
-    w.show()
-    #sys.exit(app.exec_())
-    app.exec_()
     
     
 #==============================================================================
